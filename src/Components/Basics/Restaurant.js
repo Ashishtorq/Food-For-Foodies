e07325c16647a4ebf79db9b63605101c55014321
Ashistorq <ashishsinghtarwa@gmail.com>
2023-07-11 00:42:17 +0530

    navigation bar

---
+++ src/Components/Basics/Restaurant.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+const Restaurant = () => {
+  return (
+    <div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author" subtle>
+              Breakfast
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Restaurant;
